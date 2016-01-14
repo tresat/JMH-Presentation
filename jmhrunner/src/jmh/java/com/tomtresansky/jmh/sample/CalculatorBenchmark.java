2cbab12e935e8e958e6528050a51548ce80bcf5b
Tom Tresansky <tom.tresansky@gmail.com>
2016-01-14 13:10:24 -0500

    Work on slides, making the JMH-Runner independent of the main presentation project.

---
+++ jmhrunner/src/jmh/java/com/tomtresansky/jmh/sample/CalculatorBenchmark.java
@@ -0,0 +1,27 @@
+package com.tomtresansky.jmh.sample;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import com.tomtresansky.application.Calculator;
+
+/**
+ * A sample benchmark class for the {@link Calculator} class using JMH.
+ * <p>
+ * Note that {@link Benchmark} classes must <strong>NOT</strong> be
+ * {@code final}.
+ *
+ * @author ttresans
+ */
+public class CalculatorBenchmark {
+  /**
+   * Demonstrates running a benchmark with JMH for the
+   * {@link Calculator#distance(double, double, double, double)} method
+   * 
+   * @return having the {@link Benchmark} method return a value prevents
+   *         dead-code removal at run time
+   */
+  @Benchmark
+  public double distanceFormulaBenchmark() {
+    return Calculator.distance(0.0d, 0.0d, 10.0d, 10.0d);
+  }
+}
